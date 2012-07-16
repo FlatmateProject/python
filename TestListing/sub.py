1c74db7843bcd352fa0d5d22cd48f8d050bba512
piotro <piotro87@vp.pl>
2012-07-16 18:34:09 +0200

    I have finshed substract algoritm.

--- TestListing/sub.py
+++ TestListing/sub.py
@@ -1,15 +1,21 @@
 import os
 import sys
 
+def isNotIn(local, currentTestsList):
+    for item in currentTestsList:
+        if item.find(local) != -1:
+           return False
+    return True
+
 if len(sys.argv) > 2:
     currentTestsFile = open(sys.argv[1], "r")
     currentTestsList = currentTestsFile.readlines()
     betterTestsFile = open(sys.argv[2], "r")
     betterTestsList = betterTestsFile.readlines()
+    result = list()
     for item in betterTestsList:
         local = item[item.rfind(".") + 1:item.find("=")]
-	local = "\t" + local + ".class"
-	print local
-    result = list()
+	if isNotIn(local, currentTestsList):
+	    result.append(item);
     for testName in result:
         print testName.replace("\n", "") 
