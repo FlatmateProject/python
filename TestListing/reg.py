bef15bb23d8f5ba1493c3732d42893c437bbaf3f
piotro <piotro87@vp.pl>
2012-07-19 22:36:11 +0200

    I have created algoritm to search regular expression in file

---
+++ TestListing/reg.py
@@ -0,0 +1,10 @@
+import os
+import sys
+
+if len(sys.argv) > 2:
+    currentTestsFile = open(sys.argv[1], "r")
+    currentTestsList = currentTestsFile.readlines()
+    print sys.argv[2]
+    result = [item for item in currentTestsList if item.find(sys.argv[2]) == -1]
+    for testName in result:
+        print testName.replace("\n", "") 
