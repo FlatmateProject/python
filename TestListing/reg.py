ec521c1ead2a2b0545512deae1407a5e4a402a49
piotro <piotro87@vp.pl>
2012-07-19 22:50:51 +0200

    fix

--- TestListing/reg.py
+++ TestListing/reg.py
@@ -4,7 +4,6 @@ import sys
 if len(sys.argv) > 2:
     currentTestsFile = open(sys.argv[1], "r")
     currentTestsList = currentTestsFile.readlines()
-    print sys.argv[2]
     result = [item for item in currentTestsList if item.find(sys.argv[2]) == -1]
     for testName in result:
         print testName.replace("\n", "") 
